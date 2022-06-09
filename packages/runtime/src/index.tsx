669da75f9d6a3787f2def68bd7716c8cfd6cb543
Kirill Rogovoy <flash.kirill@gmail.com>
2022-06-09 12:59:01 +0300

    update

--- packages/runtime/src/index.tsx
+++ packages/runtime/src/index.tsx
@@ -1,11 +1,16 @@
 import ReactDOM from 'react-dom/client'
-import './styles.css'
+import styles from './styles.css'
 import { ImpulseParams, ImpulseRoot } from './app'
 
 export function mountApp(params?: ImpulseParams) {
   if (typeof window === 'undefined') {
     return
   }
+
+  const styleElement = document.createElement('style')
+  styleElement.id = 'impulse-styles'
+  styleElement.innerHTML = styles
+  document.head.appendChild(styleElement)
 
   const rootElement = document.createElement('div')
   rootElement.id = 'impulse-mount-point'
