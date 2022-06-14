873a76bee36eb6410b12c19615ee416c62cb8d28
Kirill Rogovoy <flash.kirill@gmail.com>
2022-06-14 11:50:59 +0300

    update deps

--- packages/runtime/dev-app/vite.config.ts
+++ packages/runtime/dev-app/vite.config.ts
@@ -4,11 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      'prettier-config': '../../../.prettierrc.js'
-    }
-  },
   optimizeDeps: {
     include: ['../../../.prettierrc.js'],
   },
