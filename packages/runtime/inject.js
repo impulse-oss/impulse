e9113feaf4b6d3d8c5d122a826fb810ca3816b3d
Kirill Rogovoy <flash.kirill@gmail.com>
2022-08-01 15:37:09 +0300

    fix cdn install flow

--- packages/runtime/inject.js
+++ packages/runtime/inject.js
@@ -1,5 +1,5 @@
 d = document
 s = d.createElement('script')
-s.src = `https://cdn.jsdelivr.net/npm/@impulse.dev/runtime@latest/impulse.iife.js?${Date.now()}`
+s.src = `https://cdn.jsdelivr.net/npm/@impulse.dev/runtime@latest/dist/impulse.iife.js?${Date.now()}`
 d.body.appendChild(s)
 s.onload = () => IMPULSE_RUN()
